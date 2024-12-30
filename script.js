4588c6dd66bd57f07fb020285b0208821c39efbf
Rajashree Suvarna <rajashreebkr@gmail.com>
2024-12-30 15:51:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const apiKey = '89ecfc285e4c1becc2dbdd863dc046a1'; 
+
+document.getElementById('submitBtn').addEventListener('click', async () => {
+    const city = document.getElementById('cityInput').value;
+    const weatherInfo = document.getElementById('weatherInfo');
+    const errorMessage = document.getElementById('errorMessage');
+    weatherInfo.classList.add('hidden');
+    errorMessage.classList.add('hidden');
+
+    if (!city) {
+        errorMessage.textContent = "Please enter a city name.";
+        errorMessage.classList.remove('hidden');
+        return;
+    }
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error("City not found.");
+        }
+
+        const data = await response.json();
+        document.getElementById('cityName').textContent = data.name;
+        document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C`;
+        document.getElementById('description').textContent = `Weather: ${data.weather[0].description}`;
+        document.getElementById('weatherIcon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+        weatherInfo.classList.remove('hidden');
+    } catch (error) {
+        errorMessage.textContent = error.message;
+        errorMessage.classList.remove('hidden');
+    }
+});
+
+
+
+
